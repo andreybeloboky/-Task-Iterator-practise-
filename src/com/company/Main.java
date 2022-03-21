bcacfd95cf41a7396481a28178b603117827b313
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-03-21 14:02:47 +0300

    add practise of task with ArrayList, HashSet and HashMap

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,46 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> exampleOne = new ArrayList<String>();
+        exampleOne.add("One");
+        exampleOne.add("Two");
+        exampleOne.add("Three");
+        exampleOne.add("One");
+        Iterator<String> iteratorOne = exampleOne.iterator();
+        while (iteratorOne.hasNext()) {
+            String value = iteratorOne.next();
+            if (value == "Two") {
+                iteratorOne.remove();
+            }
+        }
+        for (String s : exampleOne) {
+            System.out.println(s);
+        }
+
+        HashSet<String> exampleTwo = new HashSet<>();
+        exampleTwo.add("One");
+        exampleTwo.add("Two");
+        exampleTwo.add("Three");
+        Iterator<String> iteratorTwo = exampleTwo.iterator();
+        System.out.println();
+        while (iteratorTwo.hasNext()) {
+            System.out.println(iteratorTwo.next());
+        }
+
+
+        Map<String, Integer> exampleThree = new HashMap<>();
+        exampleThree.put("One", 1);
+        exampleThree.put("Two", 1);
+        exampleThree.put("Three", 1);
+        Iterator<String> iteratorThee = exampleThree.keySet().iterator();
+        System.out.println();
+        while (iteratorThee.hasNext()) {
+            String value = iteratorThee.next();
+            System.out.println(value);
+        }
     }
 }
